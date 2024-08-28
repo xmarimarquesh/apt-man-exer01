b39c653b22e215d8add6e9b0a18606c6fa6c91be
xmarimarquesh <marimrquesh39@gmail.com>
2024-08-28 16:40:39 -0300

    FEATURE 01

--- Login/script.js
+++ Login/script.js
@@ -1,18 +1,38 @@
+
+
 function login() {
   var nome = $("#nome").val();
   var senha = $("#senha").val();
 
+  const card = document.createElement("div");
+  card.className = "card";
+  card.style.display = "block";
+  card.style.width = "30%";
+  card.style.color = "white";
+  card.style.padding = "20px";
+  card.style.textAlign = "center";
+
+  const btnFechar = document.createElement("a");
+  btnFechar.href = "index.html";
+  btnFechar.textContent = "Fechar";
+
+  document.body.appendChild(card);
+  
   if (nome && senha && nome === "admin" && senha === "admin") {
     const user = {
       name: nome,
       dataEntrada: new Date(),
       id: Math.floor(Math.random() * 100000),
     };
-
+    
     localStorage.setItem("usuario", JSON.stringify(user));
-
+    
+    // Redireciona para a página da loja
     window.location.href = "../Loja/loja.html";
+    
   } else {
-    //logica para se o nome e senha forem incorretos
+    card.style.background = "red";
+    card.textContent = "Nome ou senha incorretos. ";
+    card.appendChild(btnFechar);
   }
 }
